f07b608a9394218e1094e14179255dbf3396e1c3
viiviii <viiiix.viiviii@gmail.com>
2022-04-13 20:27:18 +0900

    feat: Host 역할 추가

---
+++ src/main/java/baseball/AIHost.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+// TODO: 클래스명 - LocalHost? ComputerHost? AutoHost? AIHost?
+// TODO: immutable 해야하나? 사람은 그대로니까 mutable 한가?
+// TODO: 검증 완료 된 gameNumbers와 검증 안된 List<Integer> 중 무얼 갖고 있어야할까? -> 누가 유효성 검증?
+public class AIHost implements Host {
+
+    private List<Integer> numbers;
+
+    public void think() {
+        numbers = pickRandomUniqueNumbers();
+    }
+
+    private List<Integer> pickRandomUniqueNumbers() {
+        final int start = 1; // TODO
+        final int end = 9;
+        final int count = 3;
+        return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+}
