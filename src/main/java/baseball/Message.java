3e976c421bfa54e51e3215765ab1044efe4cc0a2
viiviii <viiiix.viiviii@gmail.com>
2022-04-15 16:51:15 +0900

    refactor: Hint 이름은 Message가 정하도록

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -17,17 +17,33 @@ public class Message {
 
     public String toHint() {
         if (score.isNothing()) {
-            return NOTHING.getName();
+            return hintName(NOTHING);
         }
         final List<Hint> hints = sortedOutputHints();
         String message = "";
         for (Hint hint : hints) {
-            message += score.get(hint) + hint.getName() + " ";
+            message += score.get(hint) + hintName(hint) + " ";
         }
         return message.trim();
     }
 
     private List<Hint> sortedOutputHints() {
         return Arrays.asList(BALL, STRIKE);
+    }
+
+    public String hintName(Hint hint) {
+        String name = "";
+        switch (hint) {
+            case STRIKE:
+                name = "스트라이크";
+                break;
+            case BALL:
+                name = "볼";
+                break;
+            case NOTHING:
+                name = "낫싱";
+                break;
+        }
+        return name;
     }
 }
--- src/test/java/baseball/MessageTest.java
+++ src/test/java/baseball/MessageTest.java
@@ -2,9 +2,10 @@ package baseball;
 
 import baseball.score.Score;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import static baseball.Hint.BALL;
-import static baseball.Hint.STRIKE;
+import static baseball.Hint.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -12,6 +13,7 @@ import static org.mockito.Mockito.mock;
 class MessageTest {
 
     private Score score = mock(Score.class);
+    private Message message = new Message(score);
 
     @Test
     void 볼과_스트라이크가_함께_있는_경우_볼_힌트가_먼저_온다() {
@@ -19,13 +21,35 @@ class MessageTest {
         final int count = 1;
         given(score.get(BALL)).willReturn(count);
         given(score.get(STRIKE)).willReturn(count);
-        Message message = new Message(score);
 
         //when
         String hint = message.toHint();
 
         //then
-        String expected = String.format("%d%s %d%s", count, BALL.getName(), count, STRIKE.getName());
+        String expected = String.format("%d%s %d%s", count, "볼", count, "스트라이크");
         assertThat(hint).isEqualTo(expected);
+    }
+
+    @Test
+    void hintNames() {
+        // when
+        String strike = message.hintName(STRIKE);
+        String ball = message.hintName(BALL);
+        String nothing = message.hintName(NOTHING);
+
+        //then
+        assertThat(strike).isEqualTo("스트라이크");
+        assertThat(ball).isEqualTo("볼");
+        assertThat(nothing).isEqualTo("낫싱");
+    }
+
+    @ParameterizedTest
+    @EnumSource(Hint.class)
+    void 모든_hint는_이름이_있다(Hint hint) {
+        //when
+        String hintName = message.hintName(hint);
+
+        //then
+        assertThat(hintName).isNotBlank();
     }
 }
