db941847a4cc40a18ad0a0c45695c5f19941161e
viiviii <viiiix.viiviii@gmail.com>
2022-04-15 15:46:18 +0900

    refactor: Message가 Score를 사용하도록

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,39 +1,31 @@
 package baseball;
 
-import java.util.ArrayList;
+import baseball.score.Score;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Message {
 
-    private final Map<HintStatus, Integer> hintCountMap;
+    private final Score score;
 
-    public Message(Map<HintStatus, Integer> hintCountMap) {
-        this.hintCountMap = hintCountMap;
+    public Message(Score score) {
+        this.score = score;
     }
 
     public String toHint() {
-        if (containsAllNothing()) {
+        if (score.isNothing()) {
             return HintStatus.NOTHING.getName();
         }
         final List<HintStatus> hints = hintMessageOrder();
         String hintMessage = "";
         for (HintStatus hint : hints) {
-            hintMessage += hintCountMap.get(hint) + hint.getName() + " ";
+            hintMessage += score.get(hint) + hint.getName() + " ";
         }
         return hintMessage.trim();
     }
 
-    private boolean containsAllNothing() {
-        final Integer nothingCount = hintCountMap.getOrDefault(HintStatus.NOTHING, 0);
-        return nothingCount == 3; // TODO: 하드코딩 제거
-    }
-
     private List<HintStatus> hintMessageOrder() {
-        final List<HintStatus> printableHintOrder = Arrays.asList(HintStatus.BALL, HintStatus.STRIKE);
-        final List<HintStatus> hints = new ArrayList<>(printableHintOrder);
-        hints.retainAll(hintCountMap.keySet());
-        return hints;
+        return Arrays.asList(HintStatus.BALL, HintStatus.STRIKE);
     }
 }
