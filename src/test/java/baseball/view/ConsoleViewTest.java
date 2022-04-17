36c92f6d6e51fa9e20f4ca23800339cbe146beb7
viiviii <viiiix.viiviii@gmail.com>
2022-04-17 14:50:00 +0900

    test: ConsoleView 낫싱과 다른 힌트 모두 있을때 테스트 추가

--- src/test/java/baseball/view/ConsoleViewTest.java
+++ src/test/java/baseball/view/ConsoleViewTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import static baseball.Hint.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -22,7 +21,7 @@ class ConsoleViewTest {
     private Score score = mock(Score.class);
     private ConsoleView view = new ConsoleView(message);
 
-    private int points = 1;
+    private int points = 2;
     private String strikeName = "스트라이크";
     private String ballName = "볼";
     private String nothingName = "낫싱";
@@ -33,14 +32,13 @@ class ConsoleViewTest {
         given(message.hintName(STRIKE)).willReturn(strikeName);
         given(message.hintName(BALL)).willReturn(ballName);
         given(message.hintName(NOTHING)).willReturn(nothingName);
-        given(score.get(any())).willReturn(points);
     }
 
     @Test
     void 낫싱만_있는_경우_힌트이름인_문자열만_리턴한다() {
         //given
-        Match match = createMatch(NOTHING, points);
-        given(score.matches()).willReturn(asList(match));
+        given(score.isNothing()).willReturn(true);
+        given(score.get(NOTHING)).willReturn(points);
 
         //when
         String message = view.scoreMessage(score);
@@ -54,6 +52,7 @@ class ConsoleViewTest {
         //given
         Match match = createMatch(STRIKE, points);
         given(score.matches()).willReturn(asList(match));
+        given(score.get(STRIKE)).willReturn(points);
 
         //when
         String message = view.scoreMessage(score);
@@ -67,6 +66,7 @@ class ConsoleViewTest {
         //given
         Match match = createMatch(BALL, points);
         given(score.matches()).willReturn(asList(match));
+        given(score.get(BALL)).willReturn(points);
 
         //when
         String message = view.scoreMessage(score);
@@ -81,12 +81,30 @@ class ConsoleViewTest {
         Match strikeMatch = createMatch(STRIKE, points);
         Match ballMatch = createMatch(BALL, points);
         given(score.matches()).willReturn(asList(strikeMatch, ballMatch));
+        given(score.get(STRIKE)).willReturn(points);
+        given(score.get(BALL)).willReturn(points);
 
         //when
         String message = view.scoreMessage(score);
 
         //then
         assertThat(message).isEqualTo(points + ballName + " " + points + strikeName);
+    }
+
+    @Test
+    void 낫싱이_다른_힌트와_함께_있는_경우_낫싱은_출력되지_않는다() {
+        //given
+        Match strikeMatch = createMatch(STRIKE, points);
+        Match strikeNothing = createMatch(NOTHING, points);
+        given(score.matches()).willReturn(asList(strikeNothing, strikeMatch));
+        given(score.get(STRIKE)).willReturn(points);
+        given(score.get(NOTHING)).willReturn(points);
+
+        //when
+        String message = view.scoreMessage(score);
+
+        //then
+        assertThat(message).isEqualTo(points + strikeName);
     }
 
     private Match createMatch(Hint hint, int points) {
