eeb478c250c484e4c1de2b11f203d7a498770b48
viiviii <viiiix.viiviii@gmail.com>
2022-04-19 18:26:39 +0900

    feat: 게임 숫자와 위치를 나타내는 Ball 클래스 생성

---
+++ src/main/java/baseball/model/Ball.java
@@ -0,0 +1,36 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class Ball {
+    private final int position;
+    private final BallNumber number;
+
+    public Ball(int position, int number) {
+        this.position = position;
+        this.number = new BallNumber(number);
+    }
+
+    public Hint matchOf(Ball other) {
+        if (this.equals(other)) {
+            return Hint.STRIKE;
+        }
+        if (this.number.equals(other.number)) {
+            return Hint.BALL;
+        }
+        return Hint.NOTHING;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ball)) return false;
+        Ball ball = (Ball) o;
+        return position == ball.position && number.equals(ball.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, position);
+    }
+}
