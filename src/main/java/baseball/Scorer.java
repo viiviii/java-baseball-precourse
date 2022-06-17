97a621b76649a858b1d5f557bbbfa040b96b7534
viiviii <viiiix.viiviii@gmail.com>
2022-06-17 18:08:22 +0900

    :sparkles: 숫자 2개와 1개를 비교한 점수 구현

--- src/main/java/baseball/Scorer.java
+++ src/main/java/baseball/Scorer.java
@@ -4,23 +4,34 @@ import static baseball.Score.BALL;
 import static baseball.Score.NOTHING;
 import static baseball.Score.STRIKE;
 
+import java.util.List;
+
 public final class Scorer {
 
-    public Score scoreTo(Ball ball, Ball other) {
-        if (isStrike(ball, other)) {
+    public Score scoreTo(List<Ball> balls, Ball other) {
+        if (isStrike(balls, other)) {
             return STRIKE;
         }
-        if (isBall(ball, other)) {
+        if (isBall(balls, other)) {
             return BALL;
         }
         return NOTHING;
     }
 
-    public boolean isStrike(Ball ball, Ball other) {
-        return ball.equals(other);
+    public boolean isStrike(List<Ball> balls, Ball other) {
+        return balls.contains(other);
     }
 
-    public boolean isBall(Ball ball, Ball other) {
-        return !ball.equals(other) && ball.hasSameNumber(other);
+    public boolean isBall(List<Ball> balls, Ball other) {
+        return !isStrike(balls, other) && hasSameNumber(balls, other);
+    }
+
+    public boolean hasSameNumber(List<Ball> balls, Ball other) {
+        for (Ball ball : balls) {
+            if (ball.hasSameNumber(other)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/baseball/ScorerTest.java
+++ src/test/java/baseball/ScorerTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,45 +11,68 @@ class ScorerTest {
 
     private final Scorer scorer = new Scorer();
 
-    @ParameterizedTest(name = "strike: position={0}, expected={1}")
-    @CsvSource(value = {"1, true", "2, false", "3, false"})
-    void strike(int position, boolean expected) {
-        //given
-        Ball ball = new Ball(1, 1);
-        Ball other = new Ball(position, 1);
-
-        //when
-        boolean actual = scorer.isStrike(ball, other);
-
-        //then
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @ParameterizedTest(name = "ball: position={0}, expected={1}")
-    @CsvSource(value = {"1, false", "2, true", "3, true"})
-    void ball(int position, boolean expected) {
-        //given
-        Ball ball = new Ball(1, 1);
-        Ball other = new Ball(position, 1);
-
-        //when
-        boolean actual = scorer.isBall(ball, other);
-
-        //then
-        assertThat(actual).isEqualTo(expected);
-    }
-
     @ParameterizedTest(name = "score: position={0}, number={1}, expected={2}")
-    @CsvSource(value = {"1, 1, STRIKE", "2, 1, BALL", "2, 2, NOTHING"})
+    @CsvSource(value = {
+            "1, 1, STRIKE",
+            "1, 2, BALL",
+            "1, 3, NOTHING"})
     void score(int position, int number, Score expected) {
         //given
-        Ball ball = new Ball(1, 1);
+        List<Ball> balls = createBalls(1, 2); // 12
         Ball other = new Ball(position, number);
 
         //when
-        Score actual = scorer.scoreTo(ball, other);
+        Score actual = scorer.scoreTo(balls, other);
 
         //then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "strike: position={0}, number={1}, expected={2}")
+    @CsvSource(value = {"1, 1, true", "1, 2, false", "2, 1, false"})
+    void strike(int position, int number, boolean expected) {
+        //given
+        List<Ball> balls = createBalls(1, 2);
+        Ball other = new Ball(position, number);
+
+        //when
+        boolean actual = scorer.isStrike(balls, other);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "ball: position={0}, number={1}, expected={2}")
+    @CsvSource(value = {"1, 1, false", "1, 2, true", "2, 1, true"})
+    void ball(int position, int number, boolean expected) {
+        //given
+        List<Ball> balls = createBalls(1, 2);
+        Ball other = new Ball(position, number);
+
+        //when
+        boolean actual = scorer.isBall(balls, other);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "hasSameNumber: position={0}, number={1}, expected={2}")
+    @CsvSource(value = {"1, 1, true", "1, 2, true", "1, 3, false"})
+    void hasSameNumber(int position, int number, boolean expected) {
+        //given
+        List<Ball> balls = createBalls(1, 2);
+        Ball other = new Ball(position, number);
+
+        //when
+        boolean actual = scorer.hasSameNumber(balls, other);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private List<Ball> createBalls(int number1, int number2) {
+        Ball ball1 = new Ball(1, number1);
+        Ball ball2 = new Ball(2, number2);
+        return Arrays.asList(ball1, ball2);
     }
 }
