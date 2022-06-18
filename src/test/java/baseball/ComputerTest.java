1cafcef265a15b59ec11f0114b4eb5ac385bee53
viiviii <viiiix.viiviii@gmail.com>
2022-06-18 15:11:51 +0900

    ✨ 랜덤한 야구 숫자 만들기 (#26)
    * :poop: 중복 없는 세자리 숫자 만들기 구현
    
    * :recycle: MyRandoms 클래스 추출
    
    * :recycle: 기능을 Computer 클래스로 추출

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import baseball.util.MyRandoms;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+    private MyRandoms randoms = mock(MyRandoms.class);
+    private Computer computer = new Computer(randoms);
+
+    @DisplayName("랜덤한 야구 숫자를 생성한다")
+    @Test
+    void createRandomBallNumbers() {
+        //given
+        given(randoms.pickNumberInRange(anyInt(), anyInt())).willReturn(1, 2, 1, 3, 4);
+
+        //when
+        List<Integer> randomBallNumbers = computer.randomBallNumbers();
+
+        //then
+        assertThat(randomBallNumbers).containsExactlyInAnyOrder(1, 2, 3);
+    }
+}
