93b9555c737b5f7c6f3dab2fa4c5df80c8082eb3
viiviii <viiiix.viiviii@gmail.com>
2022-06-18 17:28:03 +0900

    :white_check_mark: 사용자에게 값을 입력받아 판별 결과를 출력하는 테스트 추가

--- src/test/java/baseball/controller/ControllerTest.java
+++ src/test/java/baseball/controller/ControllerTest.java
@@ -4,6 +4,7 @@ import static baseball.Score.STRIKE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import baseball.Computer;
 import baseball.Score;
@@ -16,8 +17,28 @@ import org.junit.jupiter.api.Test;
 class ControllerTest {
 
     private final InputView inputView = mock(InputView.class);
+    private final OutputView outputView = mock(OutputView.class);
     private final Computer computer = mock(Computer.class);
     private final Controller controller = new Controller(inputView, computer);
+
+    @DisplayName("사용자에게 값을 입력받아 컴퓨터의 값과 비교한 결과를 사용자에게 출력한다")
+    @Test
+    void play() {
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        given(inputView.ballNumbers()).willReturn(numbers);
+        given(computer.ballNumbers()).willReturn(numbers);
+
+        //when
+        controller.play();
+
+        //then
+        verify(outputView).selectNumberRequest();
+        verify(computer).ballNumbers();
+        verify(inputView).ballNumbers();
+        verify(outputView).selectNumberResponse();
+
+    }
 
     @DisplayName("사용자에게 입력받은 값과 컴퓨터의 값을 비교하기")
     @Test
